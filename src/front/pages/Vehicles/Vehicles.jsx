877ed059597a63d95859b2946d535881595b3078
MsterX2 <167225733+MsterX2@users.noreply.github.com>
2025-12-15 18:25:47 +0000

    Refactor Vehicles component to fetch data from API and update filtering logic

--- src/front/pages/Vehicles/Vehicles.jsx
+++ src/front/pages/Vehicles/Vehicles.jsx
@@ -18,76 +18,19 @@ export const Vehicles = () => {
     }
     useEffect(
         () => {
-            // getData()
-            setVehiclesData(
-                [
-                    {
-                        id: 1,
-                        title: 'X-Wing Starfighter',
-                        image: 'https://images.unsplash.com/photo-1656716459789-9875ab1177f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxzdGFyJTIwd2FycyUyMHNwYWNlfGVufDF8fHx8MTc2NDc3ODU0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-                        items: ['Clase: Caza estelar',
-                            'Fabricante: Incom Corporation',
-                            'Armamento: 4 cañones láser',
-                            'Tripulación: 1 piloto + 1 droide']
-                    },
-                    {
-                        id: 2,
-                        title: 'Millennium Falcon',
-                        image: 'https://images.unsplash.com/photo-1656716459789-9875ab1177f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxzdGFyJTIwd2FycyUyMHNwYWNlfGVufDF8fHx8MTc2NDc3ODU0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-                        items: ['Clase: Carguero ligero YT-1300',
-                            'Velocidad: 0.5 hiperespacial',
-                            'Propietario: Han Solo',
-                            'Características: Modificado ilegalmente']
-                    },
-                    {
-                        id: 3,
-                        title: 'TIE Fighter',
-                        image: 'https://images.unsplash.com/photo-1656716459789-9875ab1177f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxzdGFyJTIwd2FycyUyMHNwYWNlfGVufDF8fHx8MTc2NDc3ODU0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-                        items: ['Clase: Caza estelar',
-                            'Afiliación: Imperio Galáctico',
-                            'Armamento: 2 cañones láser',
-                            'Característica: Alta maniobrabilidad']
-                    },
-                    {
-                        id: 4,
-                        title: 'Star Destroyer',
-                        image: 'https://images.unsplash.com/photo-1656716459789-9875ab1177f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxzdGFyJTIwd2FycyUyMHNwYWNlfGVufDF8fHx8MTc2NDc3ODU0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-                        items: ['Clase: Destructor Estelar Clase Imperial',
-                            'Longitud: 1,600 metros',
-                            'Tripulación: 37,000 personas',
-                            'Armamento: 60 turbolásers']
-                    },
-                    {
-                        id: 5,
-                        title: 'AT-AT Walker',
-                        image: 'https://images.unsplash.com/photo-1656716459789-9875ab1177f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxzdGFyJTIwd2FycyUyMHNwYWNlfGVufDF8fHx8MTc2NDc3ODU0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-                        items: ['Clase: Transporte blindado todo terreno',
-                            'Altura: 22.5 metros',
-                            'Tripulación: 5 operadores',
-                            'Capacidad: 40 soldados']
-                    },
-                    {
-                        id: 6,
-                        title: 'Speeder Bike',
-                        image: 'https://images.unsplash.com/photo-1656716459789-9875ab1177f8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=M3w3Nzg4Nzd8MHwxfHNlYXJjaHwxfHxzdGFyJTIwd2FycyUyMHNwYWNlfGVufDF8fHx8MTc2NDc3ODU0M3ww&ixlib=rb-4.1.0&q=80&w=1080&utm_source=figma&utm_medium=referral',
-                        items: ['Clase: Moto de reconocimiento',
-                            'Velocidad máxima: 500 km/h',
-                            // 'Tripulación: 1 piloto',
-                            'Uso principal: Patrulla y exploración']
-                    }
-                ])
+            getData()
         }, [])
 
-    // useEffect(
-    //     () => {
-    //         console.log(vehiclesData)
-    //         if (vehiclesData.length == 0) return;
-    //         const filtrados = vehiclesData.filter(vehicle =>
-    //             vehicle.name.toLowerCase().includes(searchTerm.toLowerCase())
-    //         );
-    //         setFilteredVehicles(filtrados)
-    //     }, [vehiclesData,]
-    // )
+    useEffect(
+        () => {
+            console.log(vehiclesData)
+            if (vehiclesData.length == 0) return;
+            const filtrados = vehiclesData.filter(vehicle =>
+                vehicle.name.toLowerCase().includes(searchTerm.toLowerCase())
+            );
+            setFilteredVehicles(filtrados)
+        }, [vehiclesData,]
+    )
 
 
 
@@ -114,9 +57,8 @@ export const Vehicles = () => {
                     filteredVehicles.map((vehicle) => (
                         <StarWarsCard
                             key={vehicle.id}
-                            image={vehicle.image}
                             title={vehicle.title}
-                            items={vehicle.items}
+                            items={[vehicle.name]}
                             onClick={() => handleVehicleClick(vehicle.id)}
                         />
                     ))
